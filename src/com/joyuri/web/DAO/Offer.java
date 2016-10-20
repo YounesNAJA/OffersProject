14de49174fa21efc68c86cb4e1d8415110939fa4
Younes NAJA <naja.younes@gmail.com>
2016-10-20 20:46:11 +0100

    MySQL Database script added
    MySQL Database script added

--- src/com/joyuri/web/DAO/Offer.java
+++ src/com/joyuri/web/DAO/Offer.java
@@ -3,7 +3,6 @@ package com.joyuri.web.DAO;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
-import org.hibernate.validator.constraints.Email;
 
 public class Offer {
 	private int id;
