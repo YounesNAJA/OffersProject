0d0ca4ac879b160e38ab2353e43d44bcdb133e4b
Younes NAJA <naja.younes@gmail.com>
2016-10-31 21:37:03 +0000

    Offers API Added (JSON)

--- src/com/joyuri/web/Controllers/OffersController.java
+++ src/com/joyuri/web/Controllers/OffersController.java
@@ -11,58 +11,73 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.google.gson.Gson;
 import com.joyuri.web.DAO.Offer;
 import com.joyuri.web.Service.OffersService;
 
 @Controller
 public class OffersController {
 	private OffersService offersService;
-	
+
 	@Autowired
 	public void setOffersService(OffersService offersService) {
 		this.offersService = offersService;
 	}
 
-	/* ============================================
-	 * Showing all offers available in the database
-	 =========================================== */
+	/*
+	 * ============================================ Showing all offers available
+	 * in the database ===========================================
+	 */
 	@RequestMapping("/offers")
-	public String home(Model model){
-		
+	public String home(Model model) {
+
 		// Getting all the offers from the service layer
 		List<Offer> offers = offersService.getCurrents();
-		
+
 		// Setting the model with a list of all offers
 		model.addAttribute("offers", offers);
 		return "offers";
 	}
-	
-	/* =======================
-	 * Create a new offer form	
-	 ====================== */
+
+	/*
+	 * ======================= Create a new offer form ======================
+	 */
 	@RequestMapping(value = "/createoffer")
-	public String createOffer(Model model){
+	public String createOffer(Model model) {
 		model.addAttribute("offer", new Offer());
 		return "createoffer";
 	}
-	
-	/* ===================================
+
+	/*
+	 * ===================================
 	 * After submitting a new offer's form
-	 ================================== */
+	 * ==================================
+	 */
 	@RequestMapping(value = "/docreate", method = RequestMethod.POST)
-	public String doCreate(Model model, @Valid Offer offer, BindingResult result){
-		
+	public String doCreate(Model model, @Valid Offer offer, BindingResult result) {
+
 		// If the form has errors on it
-		if(result.hasErrors()){
+		if (result.hasErrors()) {
 			model.addAttribute("offer", offer);
 			return "createoffer";
 		}
-		
+
 		// If the form is validated
 		else {
 			// Adding the offer to the Database
 			offersService.create(offer);
 			return "docreate";
 		}
+	}
+
+	@RequestMapping(value = "/api", method = RequestMethod.GET)
+	public String api(Model model) {
+		// Getting all the offers from the service layer
+		List<Offer> offers = offersService.getCurrents();
+
+		String offersJSON = new Gson().toJson(offers);
+		// Setting the model with a list of all offers
+		model.addAttribute("offers", offersJSON);
+		return "api";
 	}
 }
